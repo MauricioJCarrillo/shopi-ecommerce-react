1573c196845ca1a22ab8c570636cf5665c47feac
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2024-10-27 20:41:28 -0500

    Adding routes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter, RouteObject } from "react-router-dom";
 import { Home } from "./pages/Home/Home.tsx";
 import { MyAccount } from "./pages/MyAccount/MyAccount.tsx";
 import { MyOrder } from "./pages/MyOrder/MyOrder.tsx";
@@ -5,15 +6,24 @@ import { MyOrders } from "./pages/MyOrders/MyOrders.tsx";
 import { SignIn } from "./pages/SignIn/SignIn.tsx";
 import { NotFound } from "./pages/NotFound/NotFound";
 
+// El valor de retorno de useRoutes puede ser JSX.Element | null, ya que useRoutes devolverá null si no hay rutas coincidentes
+const AppRoutes = (): JSX.Element | null => {
+  const routes: RouteObject[] = [
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
+  ];
+
+  return useRoutes(routes);
+};
+
 export const App = (): JSX.Element => {
   return (
-    <>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 };
