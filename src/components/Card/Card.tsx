52024766ab4305331caf0f3a1fd62079900e8a50
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2024-10-29 20:38:13 -0500

    Adding types into Home and Card components and consuming the API to get the products information

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,23 +1,35 @@
-export const Card = (): JSX.Element => {
+interface CardProps {
+  category: string;
+  image: string;
+  price: number;
+  title: string;
+}
+
+export const Card = ({
+  category,
+  image,
+  price,
+  title,
+}: CardProps): JSX.Element => {
   return (
     <>
       <article className="h-60 w-56 cursor-pointer rounded-lg bg-white">
         <figure className="relative mb-2 h-4/5 w-full">
           <figcaption className="absolute bottom-0 left-0 m-2 rounded-lg bg-white/60 px-3 py-0.5 text-xs text-black">
-            Electronics
+            {category}
           </figcaption>
           <img
             className="h-full w-full rounded-lg object-cover"
-            src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="headphones"
+            src={image}
+            alt={title}
           />
           <button className="absolute right-0 top-0 m-2 flex h-6 w-6 items-center justify-center rounded-full bg-white p-1">
             +
           </button>
         </figure>
-        <p className="flex justify-between">
-          <span className="text-sm font-light">Headphones</span>
-          <span className="text-lg font-medium">$300</span>
+        <p className="flex items-center justify-between">
+          <span className="text-sm font-light">{title}</span>
+          <span className="text-lg font-medium">${price}</span>
         </p>
       </article>
     </>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,55 @@
+import { useEffect, useState } from "react";
 import { Card } from "../../components/Card/Card";
+import { apiUrl } from "../../api";
+
+interface ItemType {
+  id: number;
+  category: CategoryType;
+  creationAt: string; // ISO date string
+  description: string;
+  images: string[];
+  price: number;
+  title: string;
+  updatedAt: string; // ISO date string
+}
+
+interface CategoryType {
+  id: number;
+  creationAt: string; // ISO date string
+  image: string;
+  name: string;
+  updatedAt: string; // ISO date string
+}
 
 export const Home = (): JSX.Element => {
+  const [items, setItems] = useState<ItemType[]>([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/products`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data", data);
+        return setItems(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
       <h1>Home</h1>
-      <Card />
+      <section className="grid w-full max-w-screen-lg grid-cols-4 gap-4">
+        {items?.map((item: ItemType) => {
+          return (
+            <Card
+              key={item.id}
+              title={item.title}
+              category={item.category.name}
+              price={item.price}
+              image={item.images[0]}
+            />
+          );
+        })}
+      </section>
     </>
   );
 };
