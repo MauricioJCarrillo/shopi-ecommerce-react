8bdbee5e2345392fee15c9abcbec9c5ddb459341
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2025-04-05 11:26:00 -0500

    Improving styles in order card

--- src/components/OrderCard/OrderCard.tsx
+++ src/components/OrderCard/OrderCard.tsx
@@ -15,22 +15,24 @@ export const OrderCard = ({
   handleDelete,
 }: OrderCardProps): JSX.Element => {
   return (
-    <div className="mb-3 flex justify-start rounded-md border border-gray-400 p-2">
-      <figure className="h-20 w-20">
-        <img
-          className="h-full w-full rounded-lg object-cover"
-          src={imageURL}
-          alt={title}
-        />
-      </figure>
-      <p className="ml-2 flex max-w-48 flex-col">
-        <span className="text-md font-light">{title}</span>
-        <span className="text-2xl font-medium">${price}</span>
-      </p>
+    <div className="mb-3 flex justify-between rounded-md border border-gray-400 p-2">
+      <section className="flex justify-between">
+        <figure className="h-20 w-20">
+          <img
+            className="h-full w-full rounded-lg object-cover"
+            src={imageURL}
+            alt={title}
+          />
+        </figure>
+        <p className="ml-2 flex max-w-48 flex-col">
+          <span className="text-md font-light">{title}</span>
+          <span className="text-2xl font-medium">${price}</span>
+        </p>
+      </section>
 
       {handleDelete && (
         <TrashIcon
-          className="h-6 w-6 cursor-pointer text-black"
+          className="h-6 w-6 cursor-pointer text-black hover:text-red-500"
           onClick={() => handleDelete(id)}
         />
       )}
