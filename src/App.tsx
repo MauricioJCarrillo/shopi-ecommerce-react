aa22c3af2bb4fe294b998d93dfd1727e16cf0f7b
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2025-04-08 17:37:36 -0500

    Adding route and rendering individual order when each order is selected in orders page

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const AppRoutes = (): JSX.Element | null => {
     { path: "/my-order", element: <MyOrder /> },
     { path: "/my-orders", element: <MyOrders /> },
     { path: "/my-orders/last", element: <MyOrder /> },
+    { path: "/my-orders/:orderId", element: <MyOrder /> },
     { path: "/sign-in", element: <SignIn /> },
     { path: "/*", element: <NotFound /> },
   ];
--- src/pages/MyOrder/MyOrder.tsx
+++ src/pages/MyOrder/MyOrder.tsx
@@ -1,60 +1,74 @@
 import { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
 import { ChevronLeftIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../context/ShoppingCartContext";
 import { OrderCard } from "../../components/OrderCard/OrderCard";
-import { Link } from "react-router-dom";
 
 export const MyOrder = (): JSX.Element => {
   const { orders } = useContext(ShoppingCartContext);
   console.log("Orders", orders);
 
+  const currentPath = window.location.pathname;
+  const lastPath = currentPath.split("/").pop();
+
+  const params = useParams();
+  const orderId = params.orderId;
+
+  const order =
+    lastPath === "last"
+      ? orders?.[orders.length - 1]
+      : orders?.find((order) => order.id === orderId);
+
   return (
     <>
-      <div className="relative mb-6 flex w-80 items-center justify-center">
-        <Link to="/my-orders" className="absolute left-0">
-          <ChevronLeftIcon className="h-6 w-6 cursor-pointer text-black" />
-        </Link>
-        <h1 className="text-2xl font-normal">My Order</h1>
-      </div>
-      <section className="flex w-80 flex-col">
-        {orders &&
-          orders.length > 0 &&
-          orders?.map((product) => {
-            return (
-              <div key={product.date.toLocaleTimeString()}>
-                <p className="flex items-center justify-between pb-2">
-                  <span className="text-2xl font-light">Order date</span>
-                  <span className="text-2xl font-bold">
-                    {product.date.toLocaleDateString()}
-                  </span>
-                </p>
+      {order ? (
+        <>
+          <div className="relative mb-6 flex w-80 items-center justify-center">
+            <Link to="/my-orders" className="absolute left-0">
+              <ChevronLeftIcon className="h-6 w-6 cursor-pointer text-black" />
+            </Link>
+            <h1 className="text-2xl font-normal">My Order</h1>
+          </div>
+          <section className="flex w-80 flex-col">
+            <div
+              key={order?.id}
+              className="mb-3 flex w-full flex-col rounded-md border border-gray-400 p-2"
+            >
+              <p className="flex items-center justify-between pb-2">
+                <span className="text-2xl font-light">Order date</span>
+                <span className="text-2xl font-bold">
+                  {order?.date.toLocaleDateString()}
+                </span>
+              </p>
 
-                <p className="mb-4 text-2xl font-light">Products:</p>
-                {product.products?.map((product) => (
-                  <OrderCard
-                    key={product.id}
-                    id={product.id}
-                    title={product.title}
-                    imageURL={product.images[0]}
-                    price={product.price}
-                  />
-                ))}
-                <p className="flex items-center justify-between pb-6">
-                  <span className="text-2xl font-light">Total Products</span>
-                  <span className="text-2xl font-bold">
-                    {product.totalProducts}
-                  </span>
-                </p>
-                <p className="flex items-center justify-between pb-6">
-                  <span className="text-2xl font-light">Total Price</span>
-                  <span className="text-2xl font-bold">
-                    ${product.totalPrice}
-                  </span>
-                </p>
-              </div>
-            );
-          })}
-      </section>
+              <p className="mb-4 text-2xl font-light">Products:</p>
+              {order?.products.map((product) => (
+                <OrderCard
+                  key={product.id}
+                  id={product.id}
+                  title={product.title}
+                  imageURL={product.images[0]}
+                  price={product.price}
+                />
+              ))}
+              <p className="flex items-center justify-between pb-6">
+                <span className="text-2xl font-light">Total Products</span>
+                <span className="text-2xl font-bold">
+                  {order?.totalProducts}
+                </span>
+              </p>
+              <p className="flex items-center justify-between pb-6">
+                <span className="text-2xl font-light">Total Price</span>
+                <span className="text-2xl font-bold">${order?.totalPrice}</span>
+              </p>
+            </div>
+          </section>
+        </>
+      ) : (
+        <>
+          <div>No existe la orden</div>
+        </>
+      )}
     </>
   );
 };
