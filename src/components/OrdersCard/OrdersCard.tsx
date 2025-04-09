27180087b32906b913e501f26e9768f3ce868e14
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2025-04-09 14:25:51 -0500

    Improving styles in Orders Card

--- src/components/OrdersCard/OrdersCard.tsx
+++ src/components/OrdersCard/OrdersCard.tsx
@@ -1,3 +1,8 @@
+import {
+  ChevronRightIcon,
+  ShoppingBagIcon,
+  CalendarDaysIcon,
+} from "@heroicons/react/24/solid";
 interface OrdersCardProps {
   id: string;
   date: Date;
@@ -12,18 +17,27 @@ export const OrdersCard = ({
   totalPrice,
 }: OrdersCardProps): JSX.Element => {
   return (
-    <div className="mb-3 flex w-full flex-col rounded-md border border-gray-400 p-2">
-      <p className="mb-4 text-2xl font-light">{`Order #${id}`}</p>
-      <p className="mb-4 flex items-center justify-between">
-        <span className="text-2xl font-light">Order date</span>
-        <span className="text-2xl font-light">{date.toLocaleDateString()}</span>
-      </p>
-      <p className="mb-4 text-2xl font-light">
-        <span>{`Total products: ${totalProducts}`}</span>
-      </p>
-      <p className="mb-4 text-2xl font-light">
-        <span>{`Total price: $${totalPrice}`}</span>
-      </p>
+    <div className="mb-3 flex w-96 cursor-pointer flex-col rounded-lg border border-black p-4">
+      <p className="mb-4 font-light">Order # {id}</p>
+
+      <div className="flex items-center justify-between">
+        <p className="flex flex-col">
+          <div className="flex items-center gap-2">
+            <CalendarDaysIcon className="h-4 w-4 text-black" />
+            <span className="font-light">{date.toLocaleDateString()}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <ShoppingBagIcon className="h-4 w-4 text-black" />
+            <span className="font-light">
+              {totalProducts} {totalProducts === 1 ? "article" : "articles"}
+            </span>
+          </div>
+        </p>
+        <p className="flex items-center gap-2">
+          <span className="text-2xl font-medium">${totalPrice} </span>
+          <ChevronRightIcon className="h-6 w-6 cursor-pointer text-black" />
+        </p>
+      </div>
     </div>
   );
 };
